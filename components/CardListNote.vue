6a93dd7017b9590500a38cf771ea538e83931623
dwizaf <ddwizaf@gmail.com>
2023-04-12 08:39:29 +0700

    modified components and pages

--- components/CardListNote.vue
+++ components/CardListNote.vue
@@ -1,22 +1,25 @@
 <template>
-    <div class="mx-2 my-3">
-        <div class="border border-1 h-[124px] w-[547px] flex flex-row items-center p-3">
-            <div class="border-right w-[37px] m-2 flex flex-col">
-                <p class="text-3xl font-semibold place-self-center">2</p>
-                <p class="text-xl place-self-center">Bulan</p>
-            </div>
-            <div class="h-[80px] w-[1px] bg-gray-300 mx-2"></div>
-            <div class="flex justify-between p-3">
-                <div>
-                    <p class="text-xl font-bold">Subject</p>
-                    <p class="text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <div class="flex my-1">
-                        <iconUserIcon class="h-5 w-5 mx-1" />
-                        <img class="rounded-full w-5 h-5 mx-1" src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="">
-                        <p class="text-gray-500 mx-1 text-xs">Nama User</p>
-                    </div>
+    <div>
+        <div @click="$emit('detail')" class="mx-2 my-3">
+            <div class="border border-1 h-[124px] w-[547px] flex flex-row items-center p-3">
+                <div class="border-right w-[37px] m-2 flex flex-col">
+                    <p class="text-3xl font-semibold place-self-center">2</p>
+                    <p class="text-xl place-self-center">Bulan</p>
                 </div>
-                <div class="bg-red-400 rounded-full text-[12px] h-6 px-3">50%</div>
+                <div class="h-[80px] w-[1px] bg-gray-300 mx-2"></div>
+                <div class="flex justify-between p-3">
+                    <div>
+                        <p class="text-xl font-bold">Subject</p>
+                        <p class="text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <div class="flex my-1">
+                            <iconUserIcon class="h-5 w-5 mx-1 opacity-50" /> 
+                            <p class="text-gray-500 mx-1 text-sm">Personal</p>
+                            <img class="rounded-full w-5 h-5 mx-1" src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="">
+                            <p class="text-gray-500 mx-1 text-sm">Nama User</p>
+                        </div>
+                    </div>
+                    <div class="bg-red-400 rounded-full text-[12px] h-6 px-3">50%</div>
+                </div>
             </div>
         </div>
     </div>
--- pages/editProfile.vue
+++ pages/editProfile.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full h-screen mx-9 m-2">
-    <div class="my-4">
+  <div class="flex w-full h-screen mx-9 m-2">
+    <div class="w-[20%] my-4 mx-2">
       <img
         src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
         alt=""
@@ -8,58 +8,60 @@
       />
       <a href="#" class="text-gray-500 ml-2">Ubah foto</a>
     </div>
-    <div class="my-2">
-        <p>Nama pengguna</p>
-        <input
-          type="text"
-          class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
-          placeholder="Nama anda"
-        />
-    </div>
-    <div class="my-2">
-        <p>Pekerjaan</p>
-        <input
-          type="text"
-          class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
-          placeholder="Pekerjaan anda"
-        />
-    </div>
-    <div class="my-2">
-        <p>Email</p>
-        <input
-          type="email"
-          class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
-          placeholder="Email anda"
-        />
-    </div>
-    <div class="my-2">
-        <p>Nomor telepon</p>
-        <input
-          type="number"
-          class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
-          placeholder="Nama anda"
-        />
-    </div>
-    <div class="my-2">
-        <p>Kata Sandi</p>
-        <input
-          type="password"
-          class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
-          placeholder="Buat kata sandi anda"
-        />
-    </div>
-    <div class="my-2">
-        <p>Ulangi kata sandi</p>
-        <input
-          type="password"
-          class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
-          placeholder="Ulangi kata sandi anda"
-        />
-    </div>
-    <div class="w-full my-3">
-        <button class="absolute w-1/4 bg-blue-500 text-white right-12 my-4 mx-4 p-2 rounded-md">
-          Simpan
-        </button>
+    <div class="w-full mx-2 my-3">
+      <div class="my-2">
+          <p>Nama pengguna</p>
+          <input
+            type="text"
+            class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
+            placeholder="Nama anda"
+          />
+      </div>
+      <div class="my-2">
+          <p>Pekerjaan</p>
+          <input
+            type="text"
+            class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
+            placeholder="Pekerjaan anda"
+          />
+      </div>
+      <div class="my-2">
+          <p>Email</p>
+          <input
+            type="email"
+            class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
+            placeholder="Email anda"
+          />
+      </div>
+      <div class="my-2">
+          <p>Nomor telepon</p>
+          <input
+            type="number"
+            class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
+            placeholder="Nama anda"
+          />
+      </div>
+      <div class="my-2">
+          <p>Kata Sandi</p>
+          <input
+            type="password"
+            class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
+            placeholder="Buat kata sandi anda"
+          />
+      </div>
+      <div class="my-2">
+          <p>Ulangi kata sandi</p>
+          <input
+            type="password"
+            class="border border-gray-300 w-2/3 p-1 mb-2 rounded-md"
+            placeholder="Ulangi kata sandi anda"
+          />
+      </div>
+      <div class="w-full my-3">
+          <button class="absolute w-1/4 bg-blue-500 text-white right-12 my-4 mx-4 p-2 rounded-md">
+            Simpan
+          </button>
+      </div>
     </div>
   </div>
 </template>
